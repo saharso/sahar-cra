08b8dea65cc180bbb55ca9c1809da253df1a1eb6
Sahar Soffer <ssoffer@medallia.com>
2021-07-31 12:09:35 +0300

    remove unides import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Suspense} from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
