003574c662078da040af1eba17fa6f492c7b41f4
Sahar Soffer <ssoffer@medallia.com>
2021-07-31 17:16:33 +0300

    Updates

--- static/js/3.cb1fb2c7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsahar-cra"]=this["webpackJsonpsahar-cra"]||[]).push([[3],{29:function(n,r,t){"use strict";t.r(r),t.d(r,"default",(function(){return c}));t(0);var a=t(2);function c(){return Object(a.jsx)("div",{children:"another location"})}}}]);
-//# sourceMappingURL=3.cb1fb2c7.chunk.js.map
---
+++ static/js/4.2aa79c77.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsahar-cra"]=this["webpackJsonpsahar-cra"]||[]).push([[4],{31:function(n,r,t){"use strict";t.r(r),t.d(r,"default",(function(){return e}));t(0);var a=t(29),c=t.n(a),s=t(2);function e(){return Object(s.jsxs)("div",{children:["another location",Object(s.jsx)("br",{}),c.a.join(["foo","fighter"]," ")]})}}}]);
+//# sourceMappingURL=4.2aa79c77.chunk.js.map
